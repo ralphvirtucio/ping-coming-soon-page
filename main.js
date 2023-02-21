d976bfc7fde88eac28532a11a569efb1c58236ae
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-21 19:54:16 +0800

    Completed Challenge

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 // Basic Submission of Form
 
-// 1. Get the value from the form
-const enteredEmail = document.querySelector('.email__input');
+// Initialize DOM elements
+const enteredEmailInput = document.querySelector('.email__input');
 const form = document.querySelector('.ping__form');
+const invalidMessage = document.querySelector('.invalid-message');
 
-// enteredEmail.addEventListener('change', (e) => {
+// Listen to the keypress for the input
+enteredEmailInput.addEventListener('keypress', (e) => {
+  // Check if the value is not empty
+  if (e.target.value !== '') {
+    // If the value is not empty hide and remove the invalid styles
+    invalidMessage.classList.add('hide');
+    enteredEmailInput.classList.remove('input__invalid');
+    enteredEmailInput.classList.remove('email__input-invalid');
+  }
+});
 
-// });
-
+// Listen to the submission of the form
 form.addEventListener('submit', (event) => {
+  // Prevent on reloading the page
   event.preventDefault();
+  // Initialize the value of the input
   const enteredEmail = event.target[0].value;
 
+  // Check if the enteredEmail is empty
   if (enteredEmail.trim() === '') {
-    console.log('email field should not be empty');
+    // If the enteredEmail is empty show and add the invalid styles
+    invalidMessage.classList.remove('hide');
+    enteredEmailInput.classList.add('input__invalid');
+    // If the enteredEmail is empty set the approriate feedback for the user
+    invalidMessage.textContent =
+      'Whoops! It looks like you forgot to add your email';
     return;
   }
 
-  if (enteredEmail.includes('@')) {
-    console.log('correct email format');
+  // Validation of the email
+  if (
+    enteredEmail.includes('@') &&
+    !enteredEmail.startsWith('@') &&
+    !enteredEmail.endsWith('@') &&
+    enteredEmail.split('@')[1].includes('.')
+  ) {
+    // If the enteredEmail is valid reset the input value
+    event.target[0].value = '';
   } else {
-    console.log('wrong email format');
+    // If the enteredEmail is invalid add the invalid styles
+    invalidMessage.classList.remove('hide');
+    enteredEmailInput.classList.add('input__invalid');
+
+    // If the enteredEmail is invalid set the approriate feedback for the user
+    invalidMessage.textContent = 'Please provide a valid email address';
   }
 });
